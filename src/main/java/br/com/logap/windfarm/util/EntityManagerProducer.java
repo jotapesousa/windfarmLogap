1c72515f9172b8393cb722530da875a43237e846
jotapesousa <jpaulo.vmsousa@yahoo.com.br>
2019-11-04 02:32:48 -0300

    Mudança de anotações no EntityManager

--- src/main/java/br/com/logap/windfarm/util/EntityManagerProducer.java
+++ src/main/java/br/com/logap/windfarm/util/EntityManagerProducer.java
@@ -9,12 +9,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-@ApplicationScoped
 public class EntityManagerProducer {
 
     @Inject
     private PersistenceProperties properties;
 
+    @ApplicationScoped
     @Produces
     public EntityManagerFactory criarFactory() {
         return Persistence.createEntityManagerFactory("windfarmPU", properties.get());
--- src/main/resources/META-INF/dados.sql
+++ src/main/resources/META-INF/dados.sql
@@ -1,3 +1,3 @@
-insert into usuario (id, login, nome, senha) values
+insert into Usuario (id, login, nome, senha) values
 (1, 'joao', 'João Paulo', '123'),
 (2, 'danilo', 'Danilo', '123');
